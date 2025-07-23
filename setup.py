bc35c88ccae14280b70dd6fe57a84db6c137eb5d
prabha-716 <prabakarankaniraman@gmail.com>
2025-07-23 16:26:26 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(  
+    name="medi_bot_GEN-AI",
+    version="0.0.0",
+    author="prabha",
+    author_email="prabakarankaniraman@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
